3babc15080dd78dc65054f27b70d6d1756427f86
usama.s <usama.shaheer@tekhqs.com>
2024-05-08 16:29:24 +0500

    chnages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import LocationIframe from "./LocationIframe";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <LocationIframe
+          width="1200"
+          height="450"
+          // style=“border:0”
+          loading="lazy"
+          allowfullscreen
+          referrerpolicy="no-referrer-when-downgrade"
+          // url={`https://www.google.com/maps/embed/v1/place?key=AIzaSyDLGitEALPVLPoPzj7XTq2uA3Us-QutdiM&q=${buildingData?.latitude},${buildingData?.longitude}`}
+          url={`https://www.google.com/maps/embed/v1/place?key=AIzaSyDLGitEALPVLPoPzj7XTq2uA3Us-QutdiM&q=${"31.468854"},${"74.451038"}`}
+        />
+      </div>
+    </>
   );
 }
 
